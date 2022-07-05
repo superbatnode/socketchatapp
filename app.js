b91685b2e8c3af90c30956c51e368774ce9206d7
superbatnode <sandeepsoni@excellencetechnologies.in>
2022-07-05 13:24:22 +0530

    all done

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require("express");
-const app = express(); 
-const http= require("http").createServer(app); 
-const {Server} = require("socket.io")
-const io = new Server(http); 
-app.use(express.static(__dirname+"/public")); 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/index.html");  
-})
-io.on("connection", (socket)=>{
-    console.log("user has connected ");
-    socket.on("chat", (msg)=> {
-        io.emit("chat", msg);
-    });
-    socket.on("disconnect", ()=> console.log("user has been disconnected"))
-})
-http.listen(3000, ()=>console.log("server's up at 3000"));
-
-
-
-
-
-
-
+const app = express();
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+io.on("connection", (socket) => {
+  console.log("user has connected ");
+  socket.on("chat", (msg) => {
+    io.emit("chat", msg);
+  });
+  socket.on("disconnect", () => console.log("user has been disconnected"));
+});
+http.listen(3000, () => console.log("server's up at 3000"));
